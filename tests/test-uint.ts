b8c570b4e3b33584aeb4df7081bb583d471f44eb
gabrielfu <hfu.gabriel@gmail.com>
2023-02-07 15:14:19 +0800

    tests: add constructor tests

--- tests/test-uint.ts
+++ tests/test-uint.ts
@@ -1,8 +1,41 @@
+import BN from "bn.js";
 import { expect } from "chai";
 import { solidity } from "..";
 import { deploySource, testMethod, testRevertMethod, testUncheckedMethod } from "./utils";
 
 describe("uint256", function () {
+    describe("constructor", function () {
+        it("should accept integer", async function () {
+            const num = Math.pow(2, 53) - 1;
+            expect(solidity.uint256(num).toString()).to.equal(num.toString());
+        });
+
+        it("should accept max safe integer", async function () {
+            const num = Math.pow(2, 53) - 1;
+            expect(solidity.uint256(num).toString()).to.equal(num.toString());
+        });
+
+        it("should not accept unsafe integer", async function () {
+            const num = Math.pow(2, 53);
+            expect(() => solidity.uint256(num)).to.throw();
+        });
+
+        it("should accept string", async function () {
+            const num = "587321402409830431832093";
+            expect(solidity.uint256(num).toString()).to.equal(num.toString());
+        });
+
+        it("should accept hex", async function () {
+            const num = 0x12345;
+            expect(solidity.uint256(num).toString()).to.equal("74565");
+        });
+
+        it("should accept BN", async function () {
+            const num = new BN(12345);
+            expect(solidity.uint256(num).toString()).to.equal("12345");
+        });
+    });
+
     describe("min() & max()", function () {
         const body = `
             function min() public pure returns (uint256) { return type(uint256).min; }
@@ -35,6 +68,15 @@ describe("uint256", function () {
             testMethod(
                 async () => contract.func(a, b),
                 () => solidity.uint256(a).add(solidity.uint256(b)).toString(),
+            )
+        });
+        it("should accept number as right operand", async function () {
+            const a = "200";
+            const b = 10;
+            const { contract } = await promise;
+            testMethod(
+                async () => contract.func(a, b),
+                () => solidity.uint256(a).add(b).toString(),
             )
         });
         it("should overflow", async function () {
@@ -73,6 +115,15 @@ describe("uint256", function () {
                 () => solidity.uint256(a).sub(solidity.uint256(b)).toString(),
             )
         });
+        it("should accept number as right operand", async function () {
+            const a = "200";
+            const b = 10;
+            const { contract } = await promise;
+            testMethod(
+                async () => contract.func(a, b),
+                () => solidity.uint256(a).sub(b).toString(),
+            )
+        });
         it("should overflow", async function () {
             const a = "12345";
             const b = "115792089237316195423570985008687907853269984665640564039457584007913129639935";
@@ -107,6 +158,15 @@ describe("uint256", function () {
             testMethod(
                 async () => contract.func(a, b),
                 () => solidity.uint256(a).mul(solidity.uint256(b)).toString(),
+            )
+        });
+        it("should accept number as right operand", async function () {
+            const a = "200";
+            const b = 10;
+            const { contract } = await promise;
+            testMethod(
+                async () => contract.func(a, b),
+                () => solidity.uint256(a).mul(b).toString(),
             )
         });
         it("should overflow", async function () {
@@ -145,6 +205,15 @@ describe("uint256", function () {
                 () => solidity.uint256(a).div(solidity.uint256(b)).toString(),
             )
         });
+        it("should accept number as right operand", async function () {
+            const a = "200";
+            const b = 10;
+            const { contract } = await promise;
+            testMethod(
+                async () => contract.func(a, b),
+                () => solidity.uint256(a).div(b).toString(),
+            )
+        });
     });
 
     describe("mod()", function () {
@@ -163,6 +232,15 @@ describe("uint256", function () {
                 () => solidity.uint256(a).mod(solidity.uint256(b)).toString(),
             )
         });
+        it("should accept number as right operand", async function () {
+            const a = "205";
+            const b = 10;
+            const { contract } = await promise;
+            testMethod(
+                async () => contract.func(a, b),
+                () => solidity.uint256(a).mod(b).toString(),
+            )
+        });
     });
 
     describe("pow()", function () {
@@ -179,6 +257,15 @@ describe("uint256", function () {
             testMethod(
                 async () => contract.func(a, b),
                 () => solidity.uint256(a).pow(solidity.uint256(b)).toString(),
+            )
+        });
+        it("should accept number as right operand", async function () {
+            const a = "200";
+            const b = 10;
+            const { contract } = await promise;
+            testMethod(
+                async () => contract.func(a, b),
+                () => solidity.uint256(a).pow(b).toString(),
             )
         });
         it("should overflow", async function () {
