9938093607b10f23fb72e71742dd6e65c8799650
Gabriel Fu <hfu.gabriel@gmail.com>
2023-02-13 20:34:44 +0800

    remove exports

--- src/sm.ts
+++ src/sm.ts
@@ -1,18 +1,10 @@
 export {
-    Uint8, Uint16, Uint24, Uint32, Uint40, Uint48, Uint56, Uint64, 
-    Uint72, Uint80, Uint88, Uint96, Uint104, Uint112, Uint120, Uint128, 
-    Uint136, Uint144, Uint152, Uint160, Uint168, Uint176, Uint184, Uint192, 
-    Uint200, Uint208, Uint216, Uint224, Uint232, Uint240, Uint248, Uint256, 
     uint8, uint16, uint24, uint32, uint40, uint48, uint56, uint64, 
     uint72, uint80, uint88, uint96, uint104, uint112, uint120, uint128, 
     uint136, uint144, uint152, uint160, uint168, uint176, uint184, uint192, 
     uint200, uint208, uint216, uint224, uint232, uint240, uint248, uint256,
 } from "./numbers/uint";
 export {
-    Int8, Int16, Int24, Int32, Int40, Int48, Int56, Int64, 
-    Int72, Int80, Int88, Int96, Int104, Int112, Int120, Int128, 
-    Int136, Int144, Int152, Int160, Int168, Int176, Int184, Int192, 
-    Int200, Int208, Int216, Int224, Int232, Int240, Int248, Int256, 
     int8, int16, int24, int32, int40, int48, int56, int64, 
     int72, int80, int88, int96, int104, int112, int120, int128, 
     int136, int144, int152, int160, int168, int176, int184, int192, 
